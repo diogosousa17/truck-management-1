44f8180fac2e466c0d8109e9d9169407955bc767
diogosousa17 <diogo170617@gmail.com>
2021-06-04 18:21:43 +0100

    added classes and getter and setters

--- src/main/java/com/projeto/Classes/Armazem.java
+++ src/main/java/com/projeto/Classes/Armazem.java
@@ -1,5 +1,40 @@
 package com.projeto.Classes;
 
 public class Armazem {
+    private String nome;
+    private String localizacao;
+
+
+    public Armazem(String nome, String localizacao) {
+        this.nome = nome;
+        this.localizacao = localizacao;
+    }
+
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return this.localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " nome='" + getNome() + "'" +
+            ", localizacao='" + getLocalizacao() + "'" +
+            "}";
+    }
+
 
 }
--- src/main/java/com/projeto/Classes/Camiao.java
+++ src/main/java/com/projeto/Classes/Camiao.java
@@ -1,5 +1,51 @@
 package com.projeto.Classes;
 
 public class Camiao {
+    private String marca;
+    private float tara;
+    private float capacidade;
+
+
+    public Camiao(String marca, float tara, float capacidade) {
+        this.marca = marca;
+        this.tara = tara;
+        this.capacidade = capacidade;
+    }
+
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public float getTara() {
+        return this.tara;
+    }
+
+    public void setTara(float tara) {
+        this.tara = tara;
+    }
+
+    public float getCapacidade() {
+        return this.capacidade;
+    }
+
+    public void setCapacidade(float capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " marca='" + getMarca() + "'" +
+            ", tara='" + getTara() + "'" +
+            ", capacidade='" + getCapacidade() + "'" +
+            "}";
+    }
+
+
   
 }
