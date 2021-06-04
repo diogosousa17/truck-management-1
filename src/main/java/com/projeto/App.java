be17c26a7b22f9309eea24e3ee9d0cad8ff9d5cd
Pedro <pedrocosta2406@protonmail.com>
2021-06-04 17:44:17 +0100

    Initial commit v2

---
+++ src/main/java/com/projeto/App.java
@@ -0,0 +1,9 @@
+package com.projeto;
+
+import com.projeto.Classes.Cais;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/projeto/Classes/Cais.java
@@ -0,0 +1,7 @@
+package com.projeto.Classes;
+
+import com.projeto.LinearNode;
+
+public class Cais {
+
+}
